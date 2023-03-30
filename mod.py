b067b4aafd755f7e4869e5b92d5973403fac2969
Arteam-tat <arteamtatarinovmeow@gmail.com>
2023-03-30 22:28:23 +0300

    new commit

--- mod.py
+++ mod.py
@@ -1,44 +1,44 @@
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship
+import sqlalchemy
+from orm import create_tables, Publisher, Book, Stock, Shop, Sale
+from sqlalchemy.orm import sessionmaker
+import json
+import os
+import settings
 
-Base = declarative_base()
+DSN = f'postgresql://{os.getenv(settings.login)}:{os.getenv(settings.password)}@localhost:5432/' \
+      f'{os.getenv(settings.db_name)}'
+engine = sqlalchemy.create_engine(DSN)
+create_tables(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+with open('fixtures.json', 'r') as data_db:
+    data = json.load(data_db)
+for model in data:
+    if model['model'] == 'publisher':
+        publisher = Publisher(id=model['pk'], name=model['fields']['name'])
+        session.add(publisher)
+    if model['model'] == 'book':
+        book = Book(id=model['pk'], title=model['fields']['title'], publisher_id=model['fields']['publisher_id'])
+        session.add(book)
+    if model['model'] == 'shop':
+        shop = Shop(id=model['pk'], name=model['fields']['name'])
+        session.add(shop)
+    if model['model'] == 'stock':
+        stock = Stock(id=model['pk'], book_id=model['fields']['book_id'], shop_id=model['fields']['shop_id'],
+                      count=model['fields']['count'])
+        session.add(stock)
+    if model['model'] == 'sale':
+        sale = Sale(id=model['pk'], price=model['fields']['price'], date_sale=model['fields']['date_sale'],
+                    stock_id=model['fields']['stock_id'], count=model['fields']['count'])
+        session.add(sale)
+    session.commit()
 
-class Publisher(Base):
-    __tablename__ = 'publisher'
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=100), unique=True)
 
-class Book(Base):
-    __tablename__ = 'book'
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=100))
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    publisher = relationship(Publisher, backref='books')
+def book_data(session_, publisher_name):
+    for book_, shop_, price, date_sale in session_.query(Book.title, Shop.name, Sale.price, Sale.date_sale)\
+            .join(Publisher.book).join(Book.stock).join(Stock.sale).join(Stock.shop)\
+            .filter(Publisher.name == publisher_name):
+        print(book_, "|", shop_, "|", price, "|", date_sale)
 
-class Shop(Base):
-    __tablename__ = 'shop'
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String, unique=True)
 
-class Stock(Base):
-    __tablename__ = 'stock'
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    count = sq.Column(sq.Integer)
-    shop = relationship(Shop, backref='stocks')
-    book = relationship(Book, backref='stocks')
-
-class Sale(Base):
-    __tablename__= 'sale'
-    id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
-    date_sale = sq.Column(sq.Date)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id", nullable=False))
-    count = sq.Column(sq.Integer)
-    stock = relationship(Stock, backref='sales')
-
-def create_tables(engine):
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-
+book_data(session, input())
--- orm.py
+++ orm.py
@@ -1,40 +1,69 @@
-import os
-import json
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from mod import create_tables, Publisher, Shop, Book, Stock, Sale
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
 
-load_dotenv()
-DSN = os.getenv("DSN")
-engine = sqlalchemy.create_engine(DSN)
-create_tables(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-with open('dz/ORM/py-homeworks-db/orm/fixtures/tests_data.json', 'r') as fd:
-    data = json.load(fd)
-
-for record in data:
-    model = {
-        'publisher': Publisher,
-        'shop': Shop,
-        'book': Book,
-        'stock': Stock,
-        'sale': Sale,
-    }[record.get('model')]
-    session.add(model(id=record.get('pk'), **record.get('fields')))
-session.commit()
-while True:
-    publ = input('Введите фамилию автора:')
-    if publ in session.query(Publisher.name):
-        for d in session.query(Publisher.name).join(Book.name).join(Shop.name).join(Sale.price).\
-                join(Sale.date_sale).filter.like(publ).all():
-            print(d)
-    else:
-        print(f'Автора - {publ}, нет в списке')
-session.commit()
+Base = declarative_base()
 
 
+class Publisher(Base):
+    __tablename__ = 'publisher'
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(30), unique=True)
+    book = relationship('Book', back_populates='publisher')
 
-session.close()
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+
+class Book(Base):
+    __tablename__ = 'book'
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(50))
+    publisher_id = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    publisher = relationship('Publisher', back_populates='book')
+    stock = relationship('Stock', back_populates='book')
+
+    def __str__(self):
+        return f'{self.id}: {self.title}'
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+    id = sq.Column(sq.Integer, primary_key=True)
+    book_id = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    shop_id = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer)
+    book = relationship('Book', back_populates='stock')
+    shop = relationship('Shop', back_populates='stock')
+    sale = relationship('Sale', back_populates='stock')
+
+    def __str__(self):
+        return f'{self.id}: {self.book_id}, {self.shop_id}'
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(30), unique=True)
+    stock = relationship('Stock', back_populates='shop')
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date)
+    stock_id = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+    count = sq.Column(sq.Integer)
+    stock = relationship('Stock', back_populates='sale')
+
+    def __str__(self):
+        return f'{self.id}: {self.price}'
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
